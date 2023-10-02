0d4e72bbc83cc0fce14aba6076870f77400070f4
Jean Rabelo <jean.rabelo@nubbi.com.br>
2023-10-02 14:33:44 -0300

    add context

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,29 @@
 import Link from 'next/link';
+import { useAuth } from '../contexts/auth.js';
 
 const Home = () => {
+  const { isLoggedIn, login, logout } = useAuth();
+
   return (
     <div>
       <h1>Welcome to My App</h1>
-      <Link href="/login">
-        <a>Login</a>
-      </Link>
-      <br />
-      <Link href="/register">
-        <a>Register</a>
-      </Link>
+      {isLoggedIn ? (
+        <>
+          <p>You are logged in.</p>
+          <button onClick={logout}>Logout</button>
+        </>
+      ) : (
+        <>
+          <Link href="/login">
+            <a>Login</a>
+          </Link>
+          <br />
+          <Link href="/register">
+            <a>Register</a>
+          </Link>
+          <button onClick={login}>Login (Mock)</button>
+        </>
+      )}
     </div>
   );
 };
